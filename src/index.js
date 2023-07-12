01981c27d599ee9c22ddf6ce4b7a6e25d06d98bf
eccselen <139304267+eccselen@users.noreply.github.com>
2023-07-12 21:06:09 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const val1 = {
+  name: "新道",
+  age: 18,
+  height: "180"
+};
+console.log(val1.name);
+
+const val2 = ["yakult", "dena"];
+val2.push("giants");
+console.log(val2);
+
+const name = `じゃけぇ`;
+const age = 19;
+
+const message1 = `私の名前は${name}です、年齢は${age}です`;
+console.log(message1);
